3adefd6c2c0ba5826b5f1e3edbce5e9d7ac20d67
Konrad <malinowski.konrad45@gmail.com>
2026-06-09 22:41:46 +0200

    fix: update portfolio URL to ct8.pl

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,2 +1,2 @@
 export const GITHUB_URL = 'https://github.com/konradxmalinowski/demo-restauracja'
-export const PORTFOLIO_URL = 'https://konradmalinowski.pl'
+export const PORTFOLIO_URL = 'http://konrad.malinowski.ct8.pl'
